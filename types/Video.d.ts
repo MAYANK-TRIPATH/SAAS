78d09960c6061f36d50d3eae3dd0b3d8b8f6c5ed
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2024-12-18 22:33:25 +0530

    finally build resolved

---
+++ types/Video.d.ts
@@ -0,0 +1,12 @@
+export interface Video {
+  
+    publicId: string;
+    title: string;
+    description: string;
+    duration: number; // in seconds
+    originalSize: number; // in bytes
+    compressedSize: number; // in bytes
+    createdAt: string; 
+  
+  }
+  
